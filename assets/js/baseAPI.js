6d20eed6ab4a4964667a65d86a033a6825d00413
hushuai <503703668@qq.com>
2020-12-08 21:47:46 +0800

    完成获取用户基本信息和退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,12 @@ $.ajaxPrefilter(function (options) {
         Authorization: localStorage.getItem('token')
     };
 
-
+    //控制用户在后台页面的访问权限(未登录要跳回登录页)
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token'); //清除token
+            location.href = 'login.html'; //跳转到登录页面
+        }
+    } //无论请求是成功还是失败都会执行的回调，常用全局成员的释放，或者页面状态的重置
 })
